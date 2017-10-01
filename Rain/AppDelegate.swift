08e4c1c4087de0af25c7bd9a073b36a323112948
Joe Holt <wynatoej@gmail.com>
2017-10-01 12:28:34 -0500

    Initial Commit

---
+++ Rain/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Rain
+//
+//  Created by Joe Holt on 10/1/17.
+//  Copyright © 2017 Joe Holt. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
